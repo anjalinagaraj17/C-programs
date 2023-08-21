34b11f3e506e444de51f8fcdfd537447af55ea70
anjalinagaraj17 <142492283+anjalinagaraj17@users.noreply.github.com>
2023-08-21 16:34:51 +0530

    Add files via upload

---
+++ p21.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    bool a,b,c,d;
+    a=true;
+    b=false;
+    c=0;
+    d=-2;
+    printf("the values are %d\n %d\n %d\n %d\n",a,b,c,d);
+    return 0;
+}
---
+++ p23.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("enter a charecter\n",a);
+    scanf("%c",&a);
+    printf("the ASCII value is %d\n",a);
+    return 0;
+}
