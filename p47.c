3cfc2868230d9bcc858c7901a7c4a216c91e6251
anjalinagaraj17 <142492283+anjalinagaraj17@users.noreply.github.com>
2023-08-22 20:42:22 +0530

    Update p47.c

--- p47.c
+++ p47.c
@@ -2,10 +2,10 @@
 int main()
 {
 
-    float x,y,m;
-    printf("enter the values of x and y\n");
-    scanf("%f%f",&x,&y);
-    m=y/x;
+    float x1,y1,x2,y2,m;
+    printf("enter the values of x1,x2, and y1,y2\n");
+    scanf("%f%f%f%f",&x1,&x2,&y1,&y2);
+    m=(y1*y2)/(x1*x2);
     printf("the slope of the line is %f\n",m);
     return 0;
 }
